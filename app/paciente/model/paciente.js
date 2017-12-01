9d7c14c5b631d25471047d52dc5cceec4d9faf71
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2017-12-01 09:43:26 -0200

    sendi data to form

--- app/paciente/model/paciente.js
+++ app/paciente/model/paciente.js
@@ -1,10 +1,10 @@
 const paciente = {
-  init: function (nome, telefone, email, data_de_nascimento, endereço) {
+  init: function (nome, telefone, email, dataDeNascimento, endereço) {
     this._id = new Date().toISOString(),
     this.nome = nome;
     this.telefone = telefone;
     this.email = email;
-    this.data_de_nascimento = data_de_nascimento;
+    this.dataDeNascimento = dataDeNascimento;
     this.endereço = endereço;
   }
 }
