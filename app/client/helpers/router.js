75826bd9c95618390b52e724517a668ab226d068
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2017-12-02 20:38:35 -0200

    bred ok

--- app/client/helpers/router.js
+++ app/client/helpers/router.js
@@ -7,7 +7,7 @@ const navegateTo = (linkId) => {
   const linkTemplate = $(`link[rel="import"]#${linkId}`)[0];
   const template = linkTemplate.import.querySelector(`template#${linkId}`);
   const clone = document.importNode(template.content, true);
-  $('#main-content').html(clone);
+  $('#main-content').html(clone).fadeIn();
 }
 
 const listenRouting = (defaultPageId) => {
@@ -15,8 +15,19 @@ const listenRouting = (defaultPageId) => {
   $('body').click((event) => {
     const section = event.target.dataset.section;
     if (section) {
+
+      $('.breadcrumb__item').removeClass('active');
+      $(`a#${section}`).removeClass('hidden').addClass('active');
+
+
+      if (section !== "link-form-pacientes") {
+        $(`a#link-form-pacientes`).addClass('hidden').remove('active');
+      }
+
+
       navegateTo(section);
-      ipcRenderer.send(IpcEventsEnum.TEMPLATE_LOADED)
+      ipcRenderer.send(IpcEventsEnum.TEMPLATE_LOADED);
+
     }
   });
 
